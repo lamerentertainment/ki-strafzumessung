48c7cd44829268a4a76adcfb990d1cd7731e99b2
lamerentertainment <63227976+lamerentertainment@users.noreply.github.com>
2024-01-31 17:21:08 +0100

    versuch, die strafmassprognose mit 30 zu multiplizieren, sofern eine Geldstrafe prognostiziert wird

--- database/templatetags/prognoseformatierung.py
+++ database/templatetags/prognoseformatierung.py
@@ -28,3 +28,29 @@ def prognosebereich_angeben(prognose_float):
 
     elif erste_ziffer_nach_dem_komma(prognose_float) in [8, 9]:
         return f'zwischen {str(math.floor(prognose_float))} und {str(math.ceil(prognose_float+2))}'
+
+@register.filter
+def prognosebereich_angeben_fuer_geldstrafe(prognose_float):
+    """Die Prognosewerte werden mit 30 multipliziert, wenn eine Geldstrafe prognostiziert wird"""
+
+    def erste_ziffer_nach_dem_komma(zahl):
+        zahl = float(zahl)
+        nachkommastelle = abs(zahl) % 1  # Extrahiere die Nachkommastelle (nur positive Werte verwenden)
+        if nachkommastelle == 0:
+            return 0  # Keine Ziffer nach dem Komma
+        else:
+            nachkommastelle *= 10
+            erste_ziffer = int(nachkommastelle)
+            return erste_ziffer
+
+    if erste_ziffer_nach_dem_komma(prognose_float) in [1, 2]:
+        return f'zwischen {str(math.floor(prognose_float-2))*30} und {str(math.ceil(prognose_float))*30}'
+
+    elif erste_ziffer_nach_dem_komma(prognose_float) in [3, 4, 5, 6, 7]:
+        return f'zwischen {str(math.floor(prognose_float-1))*30} und {str(math.ceil(prognose_float+1))*30}'
+
+    elif erste_ziffer_nach_dem_komma(prognose_float) is 0:
+        return f'zwischen {str(math.floor(prognose_float)-1.5)*30} und {str(math.ceil(prognose_float)+1.5)*30}'
+
+    elif erste_ziffer_nach_dem_komma(prognose_float) in [8, 9]:
+        return f'zwischen {str(math.floor(prognose_float))*30} und {str(math.ceil(prognose_float+2))*30}'

9ed73bc7b379586cb10e81d8d980b90765038dde
lamerentertainment <63227976+lamerentertainment@users.noreply.github.com>
2024-01-31 16:19:21 +0100

    verbesserte admin liste für betm-urteile

--- database/admin.py
+++ database/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Urteil, KIModelPickleFile, DiagrammSVG, BetmUrteil, BetmArt, Betm, Rolle, Kanton
 
+class BetmUrteilAdmin(admin.ModelAdmin):
+    list_display = ["fall_nr", "urteilsdatum", "gericht", "freiheitsstrafe_in_monaten", "anzahl_tagessaetze"]
+
 # Register your models here.
 admin.site.register(Urteil)
 admin.site.register(KIModelPickleFile)

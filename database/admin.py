06c171d004709cbc42a6cb717b892df950d55b42
lamerentertainment <63227976+lamerentertainment@users.noreply.github.com>
2024-01-31 16:24:09 +0100

    admin seite für betm-urteile übersichtlicher gemacht

--- database/admin.py
+++ database/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Urteil, KIModelPickleFile, DiagrammSVG, BetmUrteil, BetmArt, Betm, Rolle, Kanton
 
+@admin.register(BetmUrteil)
 class BetmUrteilAdmin(admin.ModelAdmin):
     list_display = ["fall_nr", "urteilsdatum", "gericht", "freiheitsstrafe_in_monaten", "anzahl_tagessaetze"]
 
@@ -8,7 +9,6 @@ class BetmUrteilAdmin(admin.ModelAdmin):
 admin.site.register(Urteil)
 admin.site.register(KIModelPickleFile)
 admin.site.register(DiagrammSVG)
-admin.site.register(BetmUrteil)
 admin.site.register(BetmArt)
 admin.site.register(Betm),
 admin.site.register(Rolle),
